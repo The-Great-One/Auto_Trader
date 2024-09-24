b8ec2afbe41bd15ea208d2eada0ed82cf12f4be8
Sahil Goel <sahilgoel187@gmail.com>
2024-09-24 12:49:14 +0530

    Updated RSI Rule 6

--- Auto_Trader/RULE_SET_6.py
+++ Auto_Trader/RULE_SET_6.py
@@ -3,7 +3,7 @@ def buy_or_sell(df, row, holdings):
     Sell if RSI is greater than 80
     """
     
-    if df['RSI'].iloc[-1] > 80:
+    if df['RSI'].iloc[-1] >= 80:
         return "SELL"
     else:
         return "HOLD"
