173709ef6f98a4dc2db613682b2695eac2f01dbb
Sahil Goel <sahilgoel187@gmail.com>
2024-10-04 08:41:13 +0530

    Reduce Sell RSI to >= 78

--- Auto_Trader/RULE_SET_6.py
+++ Auto_Trader/RULE_SET_6.py
@@ -1,9 +1,9 @@
 def buy_or_sell(df, row, holdings):
     """
-    Sell if RSI is greater than 80
+    Sell if RSI is greater than 78
     """
     
-    if df['RSI'].iloc[-1] >= 80:
+    if df['RSI'].iloc[-1] >= 78:
         return "SELL"
     else:
         return "HOLD"
