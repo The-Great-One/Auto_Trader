277fc980621aa098a210f4ad9b86f46458c89138
Sahil Goel <sahilgoel187@gmail.com>
2024-10-04 10:58:39 +0530

    Updater Handover to Systemd

--- Auto_Trader/updater.py
+++ Auto_Trader/updater.py
@@ -1,11 +1,11 @@
-from Auto_Trader import datetime, time, subprocess
+from Auto_Trader import datetime, time, subprocess, sys
 from Auto_Trader.my_secrets import GITHUB_PAT
 def Updater():
     while True:
         result = subprocess.run(["git", "pull", "--force", f"https://{GITHUB_PAT}@github.com/The-Great-One/Auto_Trader.git", "main"], stdout=subprocess.PIPE)
         if "send" in str(result):
             print("Updated. Restarting!")
-            subprocess.run(["systemctl", "restart", "auto_trade.service"])
+            sys.exit(1)
         else:
             print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S,%f')}] No Updates Detected!")
         time.sleep(60)
