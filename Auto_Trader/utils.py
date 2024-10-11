1cb2c6efa4feb0cada7c19d37c3e5cb41a8b1df7
Sahil Goel <sahilgoel187@gmail.com>
2024-10-11 14:06:06 +0530

    Made Rule Set Usage Modular #2

--- Auto_Trader/utils.py
+++ Auto_Trader/utils.py
@@ -10,14 +10,14 @@ logger = logging.getLogger("Auto_Trade_Logger")
 
 # Default rule set values
 DEFAULT_RULE_SETS = {
-    'RULE_SET_1': 'RULE_SET_1',
-    'RULE_SET_2': 'RULE_SET_2',
-    'RULE_SET_3': 'RULE_SET_3',
-    'RULE_SET_4': 'RULE_SET_4',
-    'RULE_SET_5': 'RULE_SET_5',
-    'RULE_SET_6': 'RULE_SET_6',
-    'RULE_SET_7': 'RULE_SET_7',
-    'RULE_SET_8': 'RULE_SET_8',
+    'RULE_SET_1': RULE_SET_1,
+    'RULE_SET_2': RULE_SET_2,
+    'RULE_SET_3': RULE_SET_3,
+    'RULE_SET_4': RULE_SET_4,
+    'RULE_SET_5': RULE_SET_5,
+    'RULE_SET_6': RULE_SET_6,
+    'RULE_SET_7': RULE_SET_7,
+    'RULE_SET_8': RULE_SET_8,
     # Add more default rule sets as needed
 }
 
