2a642e680b4cc752a987e15aa5a66c3412bcaacc
Sahil Goel <sahilgoel187@gmail.com>
2024-10-21 18:57:17 +0530

    Rule 4 Sell Handover to other rules

--- Auto_Trader/RULE_SET_4.py
+++ Auto_Trader/RULE_SET_4.py
@@ -23,13 +23,13 @@ def buy_or_sell(df, row, holdings):
         return "BUY"
 
     # Sell condition with RSI threshold or MACD trigger
-    elif (
-        (df['EMA10'].iloc[-1] < df['EMA20'].iloc[-1])
-        # or ((df['RSI'].shift(1).iloc[-1] - df['RSI'].iloc[-1]) >= 3)
-        or (df['MACD_Hist'].iloc[-1] < 0)
-        or (df['MACD'].iloc[-1] < df['MACD_Signal'].iloc[-1])
-    ):
-        return "SELL"
+    # elif (
+    #     (df['EMA10'].iloc[-1] < df['EMA20'].iloc[-1])
+    #     # or ((df['RSI'].shift(1).iloc[-1] - df['RSI'].iloc[-1]) >= 3)
+    #     or (df['MACD_Hist'].iloc[-1] < 0)
+    #     or (df['MACD'].iloc[-1] < df['MACD_Signal'].iloc[-1])
+    # ):
+    #     return "SELL"
 
     else:
         return "HOLD"
